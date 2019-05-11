0c25a0abba1b39a8338da8675ef45e52dc9ef4d4
bdcostin <bailey.costin@gmail.com>
2019-05-11 11:42:44 -0400

    adds nav bar

--- src/App/index.js
+++ src/App/index.js
@@ -3,10 +3,12 @@ import './App.css'
 import WelcomeMessage from './WelcomeMessage'
 import styled, { css } from 'styled-components'
 import AppLayout from './AppLayout'
+import AppBar from './AppBar'
 
 function App () {
   return (
     <AppLayout>
+      <AppBar />
       <WelcomeMessage />
     </AppLayout>
   )
